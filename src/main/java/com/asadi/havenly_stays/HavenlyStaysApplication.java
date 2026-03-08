e1b392df0da428f04ba9d94916a7139a1ca4f4cd
Asadi Raveendra <asadiraveendra021@gmail.com>
2026-03-08 12:32:08 +0530

    initial commit

---
+++ src/main/java/com/asadi/havenly_stays/HavenlyStaysApplication.java
@@ -0,0 +1,13 @@
+package com.asadi.havenly_stays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenlyStaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HavenlyStaysApplication.class, args);
+	}
+
+}
